13cca15a945b15896f8d0096dafe51cba2bed5aa
Huanhuan Huang <huanhuan.huang@betterbills.com>
2016-04-05 13:15:59 +1000

    added reducers actions

---
+++ shared/reducers/TodoReducer.js
@@ -0,0 +1,14 @@
+import Immutable from 'immutable';
+const defaultState = new Immutable.List();
+export default function todoReducer(state = defaultState, action) {
+  switch(action.type) {
+    case 'CREATE_TODO':
+      return state.concat(action.text);
+    case 'EDIT_TODO':
+      return state.set(action.id, action.text);
+    case 'DELETE_TODO':
+      return state.delete(action.id);
+    default:
+      return state;
+  }
+}
