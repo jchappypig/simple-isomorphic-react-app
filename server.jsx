afd99313ebbd8a3dd9138348f6f7a36fa727a642
Huanhuan Huang <huanhuan.huang@betterbills.com>
2016-04-05 13:08:01 +1000

    setup client for dev server

--- server.jsx
+++ server.jsx
@@ -1,20 +1,37 @@
-import express from 'express';
+import express                   from 'express';
+import React                     from 'react';
+import { renderToString }        from 'react-dom/server'
+import { RoutingContext, match } from 'react-router';
+import createLocation            from 'history/lib/createLocation';
+import routes                    from 'routes';
 const app = express();
 app.use((req, res) => {
-  const HTML = `
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="utf-8">
-      <title>Isomorphic Redux Demo</title>
-    </head>
-    <body>
-      <div id="react-view"></div>
-      <script type="application/javascript" src="/bundle.js"></script>
-    </body>
-  </html>
-  `;
+  const location = createLocation(req.url);
+  match({ routes, location }, (err, redirectLocation, renderProps) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).end('Internal server error');
+    }
+    if (!renderProps) return res.status(404).end('Not found.');
 
-  res.end(HTML);
+    const InitialComponent = (
+      <RoutingContext {...renderProps} />
+    );
+    const componentHTML = renderToString(InitialComponent);
+    const HTML = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Isomorphic Redux Demo</title>
+      </head>
+      <body>
+        <div id="react-view">${componentHTML}</div>
+        <script type="application/javascript" src="/bundle.js"></script>
+      </body>
+  </html>    
+`
+    res.end(HTML);
+  });
 });
 export default app;
